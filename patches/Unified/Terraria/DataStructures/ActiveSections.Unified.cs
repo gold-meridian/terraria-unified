cea054e849fe5ae9c15bc7eb0a84b93ade2fc08a
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-03-05 16:39:01 -0600

    feat: normalize method bodies

--- patches/Unified/Terraria/DataStructures/ActiveSections.Unified.cs
+++ patches/Unified/Terraria/DataStructures/ActiveSections.Unified.cs
@@ -6,5 +6,8 @@ namespace Terraria.DataStructures;
 
 public static partial class ActiveSections
 {
-	public static void Resize() => LastActiveTime = new uint[Main.maxTilesX / 200 + 1, Main.maxTilesY / 150 + 1];
+	public static void Resize()
+	{
+		LastActiveTime = new uint[Main.maxTilesX / 200 + 1, Main.maxTilesY / 150 + 1];
+	}
 }
--- patches/Unified/Terraria/GameContent/LeashedEntity.Unified.cs
+++ patches/Unified/Terraria/GameContent/LeashedEntity.Unified.cs
@@ -6,5 +6,8 @@ namespace Terraria.GameContent;
 
 public partial class LeashedEntity
 {
-	public static void Resize() => BySection = new SectionEntityList[Main.maxTilesX / 200 + 1, Main.maxTilesY / 150 + 1];
+	public static void Resize()
+	{
+		BySection = new SectionEntityList[Main.maxTilesX / 200 + 1, Main.maxTilesY / 150 + 1];
+	}
 }
