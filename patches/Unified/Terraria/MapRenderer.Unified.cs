2ff3598433b2aa08df1de2747b561e6aecdcfa73
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-03-05 16:45:18 -0600

    feat: more patch cleanups

--- patches/Unified/Terraria/MapRenderer.Unified.cs
+++ patches/Unified/Terraria/MapRenderer.Unified.cs
@@ -2,29 +2,29 @@
 using Terraria.DataStructures;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace Terraria
-{
+namespace Terraria;
+
     public partial class MapRenderer
     {
-		public static void Resize()
-		{
-			for (int i = 0; i < numTargetsX; i++) {
-				for (int j = 0; j < numTargetsY; j++) {
-					if (mapTarget[i, j] != null && !mapTarget[i, j].IsDisposed)
-						mapTarget[i, j].Dispose();
-				}
-			}
-			numTargetsX = Main.maxTilesX / textureMaxWidth + 1;
-			numTargetsY = Main.maxTilesY / textureMaxHeight + 1;
-			mapTarget = new RenderTarget2D[numTargetsX, numTargetsY];
-			initMap = new bool[numTargetsX, numTargetsY];
-			mapWasContentLost = new bool[numTargetsX, numTargetsY];
-			changeQueues = new List<Point16>[numTargetsX, numTargetsY];
-			for (int i = 0; i < numTargetsX; i++) {
-				for (int j = 0; j < numTargetsY; j++) {
-					changeQueues[i, j] = new List<Point16>(ChangeRefreshThreshold);
-				}
+	public static void Resize()
+	{
+		for (int x = 0; x < numTargetsX; x++) {
+			for (int y = 0; y < numTargetsY; y++) {
+				if (mapTarget[x, y] != null && !mapTarget[x, y].IsDisposed)
+					mapTarget[x, y].Dispose();
 			}
 		}
+
+		numTargetsX = Main.maxTilesX / textureMaxWidth + 1;
+		numTargetsY = Main.maxTilesY / textureMaxHeight + 1;
+		mapTarget = new RenderTarget2D[numTargetsX, numTargetsY];
+		initMap = new bool[numTargetsX, numTargetsY];
+		mapWasContentLost = new bool[numTargetsX, numTargetsY];
+		changeQueues = new List<Point16>[numTargetsX, numTargetsY];
+		for (int x = 0; x < numTargetsX; x++) {
+			for (int y = 0; y < numTargetsY; y++) {
+				changeQueues[x, y] = new List<Point16>(capacity: ChangeRefreshThreshold);
+			}
+		}
+	}
     }
-}
