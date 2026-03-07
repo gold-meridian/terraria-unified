8e1640cd7a1dddf4dddf0c77e92c46e0a5a27c57
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-03-07 05:51:08 -0600

    feat: delta providers and deltafloat/double, fix npc animations in credits sequence

---
+++ patches/Unified/Terraria/Unified/Time/DeltaFloat.cs
@@ -0,0 +1,134 @@
+﻿using System.Runtime.CompilerServices;
+using static Terraria.Unified.Time.DeltaTimeProviderExtensions;
+
+namespace Terraria.Unified.Time;
+
+public struct DeltaFloat<TProvider>(float value)
+	where TProvider : IDeltaTimeProvider<TProvider>
+{
+	public float Value = value;
+
+	public static float Dt {
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		get => DeltaTimeOverrider<TProvider>.OverrideDeltaTime ?? TProvider.ActualDeltaTime;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static implicit operator float(DeltaFloat<TProvider> v)
+		=> v.Value;
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static implicit operator DeltaFloat<TProvider>(float v)
+		=> new(v);
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static DeltaFloat<TProvider> operator +(DeltaFloat<TProvider> a, float b)
+	{
+		a.Value += b * Dt;
+		return a;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static DeltaFloat<TProvider> operator -(DeltaFloat<TProvider> a, float b)
+	{
+		a.Value -= b * Dt;
+		return a;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static DeltaFloat<TProvider> operator *(DeltaFloat<TProvider> a, float b)
+	{
+		a.Value *= b * Dt;
+		return a;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static DeltaFloat<TProvider> operator /(DeltaFloat<TProvider> a, float b)
+	{
+		a.Value /= b * Dt;
+		return a;
+	}
+
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Add(float perTick)
+	{
+		Value += perTick * Dt;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Sub(float perTick)
+	{
+		Value -= perTick * Dt;
+	}
+
+	public override string ToString()
+	{
+		return Value.ToString();
+	}
+}
+
+public struct DeltaDouble<TProvider>(double value)
+	where TProvider : IDeltaTimeProvider<TProvider>
+{
+	public double Value = value;
+
+	public static float Dt {
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		get => DeltaTimeOverrider<TProvider>.OverrideDeltaTime ?? TProvider.ActualDeltaTime;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static implicit operator double(DeltaDouble<TProvider> v)
+		=> v.Value;
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static implicit operator DeltaDouble<TProvider>(double v)
+		=> new(v);
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static DeltaDouble<TProvider> operator +(DeltaDouble<TProvider> a, double b)
+	{
+		a.Value += b * Dt;
+		return a;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static DeltaDouble<TProvider> operator -(DeltaDouble<TProvider> a, double b)
+	{
+		a.Value -= b * Dt;
+		return a;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static DeltaDouble<TProvider> operator *(DeltaDouble<TProvider> a, double b)
+	{
+		a.Value *= b * Dt;
+		return a;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static DeltaDouble<TProvider> operator /(DeltaDouble<TProvider> a, double b)
+	{
+		a.Value /= b * Dt;
+		return a;
+	}
+
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Add(double perTick)
+	{
+		Value += perTick * Dt;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Sub(double perTick)
+	{
+		Value -= perTick * Dt;
+	}
+
+	public override string ToString()
+	{
+		return Value.ToString();
+	}
+}
