4b5099d508a69b0b53790aa7e8278473741f5002
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2026-03-10 21:40:07 -0500

    feat: unsure

---
+++ patches/Unified/GoldMeridian.Tomoko/Versioning/IDeploymentProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace GoldMeridian.Tomoko.Versioning;
+
+/// <summary>
+///		Provides information about the deployment of a game.
+/// </summary>
+public interface IDeploymentProvider
+{
+	/// <summary>
+	///		Whether this is a debug build.
+	/// </summary>
+	bool IsDebugBuild { get; }
+
+	/// <summary>
+	///		
+	/// </summary>
+	string Version { get; }
+}
---
+++ patches/Unified/GoldMeridian.Tomoko/Versioning/Updating/IUpdateManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GoldMeridian.Tomoko.Versioning.Updating;
+
+/// <summary>
+///		Responsible for managing game versions, handling checking for and
+///		installing updates, and may expect to handle game launches.
+///		<br />
+///		Additionally responsible for managing game sources and installation
+///		configurations (portable install, package-managed state, etc.).
+/// </summary>
+public interface IUpdateManager : IDisposable
+{
+	/// <summary>
+	///		Whether the application is managed by an external package manager.
+	/// </summary>
+	bool IsPackageManaged { get; }
+
+	/// <summary>
+	///		Handles an initial program launch.
+	/// </summary>
+	/// <returns>
+	///		Whether the update manager expects the process to exit after
+	///		finishing.
+	///	</returns>
+	bool HandleLaunch(string[] args);
+
+	/// <summary>
+	///		Asynchronously checks for any available updates.
+	/// </summary>
+	Task<bool> CheckForUpdateAsync(CancellationToken cancellationToken = default);
+}
